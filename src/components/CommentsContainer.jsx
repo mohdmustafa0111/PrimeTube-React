b26af3024392eaec22e4ed0ab938d1d65ac5c312
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2024-08-02 18:59:37 +0530

    Nested Comments Implemented

---
+++ src/components/CommentsContainer.jsx
@@ -0,0 +1,108 @@
+import { GrLike } from "react-icons/gr";
+import { GrDislike } from "react-icons/gr";
+
+const CommentsContainer = () => {
+  const commentData = [
+    {
+      name: "Akshay Saini",
+      text: "This is the dummy text comment by user.",
+      replies: [
+        {
+          name: "Akshay Saini",
+          text: "This is the dummy text comment by user.",
+          replies: [],
+        },
+        {
+          name: "Akshay Saini",
+          text: "This is the dummy text comment by user.",
+          replies: [
+            {
+              name: "Akshay Saini",
+              text: "This is the dummy text comment by user.",
+              replies: [
+                {
+                  name: "Akshay Saini",
+                  text: "This is the dummy text comment by user.",
+                  replies: [
+                    {
+                      name: "Akshay Saini",
+                      text: "This is the dummy text comment by user.",
+                      replies: [],
+                    },
+                  ],
+                },
+                {
+                  name: "Akshay Saini",
+                  text: "This is the dummy text comment by user.",
+                  replies: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Akshay Saini",
+      text: "This is the dummy text comment by user.",
+      replies: [],
+    },
+    {
+      name: "Akshay Saini",
+      text: "This is the dummy text comment by user.",
+      replies: [],
+    },
+    {
+      name: "Akshay Saini",
+      text: "This is the dummy text comment by user.",
+      replies: [],
+    },
+    {
+      name: "Akshay Saini",
+      text: "This is the dummy text comment by user.",
+      replies: [],
+    },
+  ];
+
+  const Comment = ({ data }) => {
+    const { name, text, replies } = data;
+
+    return (
+      <div className="flex p-2 shadow-sm bg-gray-100 rounded-lg my-2">
+        <img
+          className="w-12 h-12"
+          src="https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png"
+          alt="user"
+        />
+        <div className="px-3">
+          <p className="font-bold">{name}</p>
+          <p>{text}</p>
+          <div className="flex">
+            <GrLike className="mx-2 my-2" />
+            <GrDislike className="mx-2 my-2" />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const CommentsList = ({ comments }) => {
+    return comments.map((comment, index) => (
+      <div key={index}>
+        <Comment data={comment} />
+        <div className="pl-5 ml-5 border border-l-black">
+          <CommentsList comments={comment.replies} />
+        </div>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="m-2 p-2">
+      <h1 className="text-2xl font-bold mb-2">Comments:</h1>
+      <CommentsList comments={commentData} />
+    </div>
+  );
+};
+
+export default CommentsContainer;
--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
@@ -12,17 +13,20 @@ const WatchPage = () => {
   }, []);
 
   return (
-    <div className="p-5">
-      <iframe
-        width="1100"
-        height="500"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
+    <div className="flex flex-col">
+      <div className="p-5">
+        <iframe
+          width="1100"
+          height="500"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <CommentsContainer />
     </div>
   );
 };
